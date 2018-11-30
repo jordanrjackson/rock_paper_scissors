9ef314f711d0c0d61ef772295de8e67f31170365
Jordan Jackson <jordanrjackson0275@gmail.com>
2018-11-30 15:47:06 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+var array = ["rock", "paper", "scissors"]
+
+var clickrock = document.getElementById("rock")
+var clickPaper = document.getElementById("paper")
+var clickScissors = document.getElementById("scissors")
+
+var userChoice = rock.addEventListener("click", autoChoice)
+var userChoice = paper.addEventListener("click", autoChoice2)
+var userChoice = scissors.addEventListener("click", autoChoice3)
+
+function autoChoice() {
+  var compPick = array[Math.floor(Math.random() * array.length)]
+  var userChoice = "rock"
+  compare(userChoice, compPick)
+}
+function autoChoice2() {
+  var compPick = array[Math.floor(Math.random() * array.length)]
+  var userChoice = "paper"
+  compare(userChoice, compPick)
+}
+function autoChoice3() {
+  var compPick = array[Math.floor(Math.random() * array.length)]
+  var userChoice = "scissors"
+  compare(userChoice, compPick)
+}
+
+function validateField1() {
+  var docs = document.getElementById("img1")
+  docs.setAttribute("src", "/Users/jordan/Desktop/dpl_course/week4/day5/rock_paper_scissors/rock-50x50.png")
+  docs.style.display = 'block'
+}
+function validateField2() {
+  var docs = document.getElementById("img2")
+  docs.setAttribute("src", "/Users/jordan/Desktop/dpl_course/week4/day5/rock_paper_scissors/paper-50x50.png")
+  docs.style.display = 'block'
+}
+function validateField3() {
+  var docs = document.getElementById("img3")
+  docs.setAttribute("src", "/Users/jordan/Desktop/dpl_course/week4/day5/rock_paper_scissors/scissors-50x50.png")
+  docs.style.display = 'block'
+}
+function compare(userChoice, compPick) {
+
+  if (userChoice === compPick) {
+   return document.getElementById("game").innerHTML = "The computer chose " + `${compPick}` + "!" + " It's a tie!"
+  }
+  if (userChoice === "rock") {
+    if (compPick === "scissors") {
+      return validateField1()
+      return document.getElementById("game").innerHTML = "The computer chose " + `${compPick}` + "! You win!"
+    } else {
+      return document.getElementById("game").innerHTML = "The computer chose " + `${compPick}` + "! You lose!"
+      }
+    }
+  if (userChoice === "paper") {
+    if (compPick === "rock") {
+      return validateField2()
+      return document.getElementById("game").innerHTML = "The computer chose " + `${compPick}` + "! You win!"
+    } else {
+      return document.getElementById("game").innerHTML = "The computer chose " + `${compPick}` + "! You lose!"
+      }
+    }
+  if (userChoice === "scissors") {
+    if (compPick === "rock") {
+      return document.getElementById("game").innerHTML = "The computer chose " + `${compPick}` + "! You lose!"
+    } else {
+      return validateField3()
+      return document.getElementById("game").innerHTML = "The computer chose " + `${compPick}` + "! You win!"
+    }
+  }
+}
